a9626142ce5d06cd200a0e93c74662cc9232c381
pasko-evg <pasko.evg@gmail.com>
2016-07-23 18:09:20 +0300

    Merge remote-tracking branch 'ProjectLearning/master'
    # Conflicts:
    #	.idea/ProjectLearning.iml
    #	.idea/misc.xml

---
+++ PP4E/preview/cgi_bin/cgi101.py
@@ -0,0 +1,10 @@
+#! c:\Python35\
+
+import cgi
+form = cgi.FieldStorage()
+print('Content-type: text/html\n')
+print('<title>Reply Page</title>')
+if not 'user' in form:
+    print('<h1>Who are yuo?</h1>')
+else:
+    print('<h1>Hello %s!</h1>' % cgi.escape(form['user'].value))
---
+++ PP4E/system/more.py
@@ -0,0 +1,20 @@
+"""
+Разбивает строку или тектовый файл на страницы для интерактивого просмотра
+"""
+
+
+def more(text, numlines=15):
+    lines = text.splitlines()
+    while lines:
+        chunk = lines[:numlines]
+        lines = lines[numlines:]
+        for line in chunk:
+            print(line)
+        if lines and input('More?') not in ['y', 'Y']:
+            break
+
+
+if __name__ == '__main__':
+    import sys
+
+    more(open(sys.argv[0]).read(), 10)
